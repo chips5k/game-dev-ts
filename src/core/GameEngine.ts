87e49854c0ea9e3f7a1265e9b85a42b932224cbb
Callem Pittard <callem.pittard@gmail.com>
2016-10-03 22:15:27 +1100

    fix issues with game loop/time step

--- src/core/GameEngine.ts
+++ src/core/GameEngine.ts
@@ -6,11 +6,10 @@ export default class Game {
     private state: GameState;
     private renderer: GameStateRenderer;
 
-    private startMillis: number;
-    private currentMillis: number;
-    private deltaMillis: number;
-    private updateMillis: number;
-    private accumulatedMillis: number;
+    private totalTime: number;
+    private deltaTime: number;
+    private currentTime: number;
+    private accumulatedTime: number;
 
 
     constructor(window: any, state: GameState, renderer: GameStateRenderer) {
@@ -19,60 +18,51 @@ export default class Game {
         this.state = state;
         this.renderer = renderer;
 
-        this.startMillis = this.window.performance.now();
-        this.currentMillis = this.startMillis;
-        this.deltaMillis = 0;
-        this.accumulatedMillis = 0;
-        //Target 60 updates per second
-        this.updateMillis = 1000 / 60;
+        this.totalTime = 0;
+        this.currentTime = 0;
+        this.deltaTime = 1000 / 60; //60 updates per second
+        this.accumulatedTime = 0;
     
     }
     
     run() {
-        this.requestFrame();
+        //Queue the next run first - apparantly this is the best
+        //approach for performance reasons
+        this.window.requestAnimationFrame(() => { this.run(); });
 
-        while(this.accumulatedMillis >= this.updateMillis) {
-            let diff = this.accumulatedMillis - this.updateMillis;
-            
-            if(diff >= 0) {
-                this.update();
-                this.accumulatedMillis = diff;
-            } else {
-                break;
-            }
-        }   
+        //Update timestamps
+        let previousTime = this.currentTime;
+        this.currentTime = this.window.performance.now();
 
-        this.render();
-    }
+        //Calculate the time elapsed since the last run of the loop[]
+        let timeSinceLastRun = this.currentTime - previousTime;
 
-    update() {
-        this.state.advance(this.updateMillis);
-    }
-
-    render() {
-        var tState = this.state.clone();
-        var interp = this.accumulatedMillis / 2;
-        this.renderer.render(tState);
-    }
-
-    requestFrame() {
-        
-        let prevMillis = this.currentMillis;
-        this.currentMillis = this.window.performance.now();
-        this.deltaMillis = this.currentMillis - prevMillis;
-
-        //Clamp the delta millis, to prevent spiral of death
-        //E.g only allow a max of 9~ updates before rendering
-        //Otherwise we begin lagging behind more and more
-        //and never actually catch up
-        if(this.deltaMillis > 150) {
-            this.deltaMillis = 150;
+        //Clamp the max amount of catch up time per run loop
+        //Prefer to slow the sim down consistently, rather than spiral into
+        //exponential slowness
+        if(timeSinceLastRun > 250) {
+            timeSinceLastRun = 250;
         }
 
-        this.accumulatedMillis += this.deltaMillis;
-        this.window.requestAnimationFrame(() => { this.run(); });
+        //Add on to the accumulated time tracker
+        this.accumulatedTime += timeSinceLastRun;
+        
+
+        while(this.accumulatedTime >= this.deltaTime) {
+            //Update in delta incremenets
+            this.state.advance(this.deltaTime);
+            //Consume a delta chunk of accumulated time
+            this.accumulatedTime -= this.deltaTime;
+            //Track the total simulated time
+            this.totalTime += this.deltaTime;
+        }
+        
+        
+        //Finally render the resulting frame
+        var tState = this.state.clone();
+        var interp = this.accumulatedTime / this.deltaTime;
+        this.renderer.render(tState);
     }
-
-
+    
 }
 
